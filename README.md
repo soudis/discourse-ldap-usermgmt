2197042c4d4c3eb510db13f6d9dcac5e4fc56008
Florian Humer <soudis@gmx.at>
2018-05-09 15:34:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# discourse-ldap-usermgmt
---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: ldap-usermgmt
+# about: A plugin to autoload users and groups from LDAP
+# version: 0.0.1
+# authors: Florian Humer
+
+enabled_site_setting :awesomeness_enabled
